1c4e8965b9eef9e9a1850e83d323a83153339d71
suguruokida <suguru.okida1@gmail.com>
2016-11-12 09:23:59 +0000

    課題:user-profileに戻る

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'signup',  to: 'users#new'
   
   resources :users
+  
+  resources :microposts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
