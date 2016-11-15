f641c65db6c49e8986892638cc0c25e7e685f66e
suguruokida <suguru.okida1@gmail.com>
2016-11-15 14:08:38 +0000

    課題:フォロー、フォロワーの一覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,28 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def followings
+    render 'show_follow'
+  end
+
+  def followers
+    render 'show_follow'
+  end
+  
+  def followings
+    @title = 'followings'
+    @user = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'followers'
+    @user = User.find(params[:id])
+    @users = @user.follower_users
+    render 'show_follow'
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   resources :users
   resources :microposts
   resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
