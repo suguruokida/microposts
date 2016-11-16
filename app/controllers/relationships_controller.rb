77d481a6707e17f9d86b5f787eccd4d4e3a59c8d
suguruokida <suguru.okida1@gmail.com>
2016-11-16 14:10:09 +0000

    課題:フォロー、フォロワーの一覧

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
     before_action :logged_in_user
 
   def create
-    @user = User.find(params[:followed_id])
+    @user = User.find_or_create_by(params[:followed_id])
     current_user.follow(@user)
   end
 
